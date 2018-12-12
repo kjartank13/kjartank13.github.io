7495bd2aa64cc7baee0c0956618fcf655032d55f
Kjartan Valur <kjartank13@ru.is>
2018-12-12 20:33:59 +0000

    bunch stuff

--- js/index.js
+++ js/index.js
@@ -1,9 +1,34 @@
-// function sizeTextAreas() {
-// 	$('.text').each(function() {
-// 		console.log($(this).height());
-// 	});
-// }
+var TEXT_PADDING = 100;
+var MIN_TEXT_HEIGHT = 400;
 
-// $(function() {
-// 	sizeTextAreas();
-// });
+function setTextAreaHeight() {
+	$('.section.static.text').each(function() {
+		var newHeight = 0;
+		for (let c of $(this).children()) {
+			newHeight += $(c).height();
+		}
+		newHeight += TEXT_PADDING;
+		$(this).height(newHeight < MIN_TEXT_HEIGHT ? MIN_TEXT_HEIGHT : newHeight);
+		$(this).css('padding-top', (TEXT_PADDING));
+	});
+}
+
+
+function setParallaxHeight() {
+	let canaryHeight = $('#canary').height();
+	$('.section.parallax').each(function() {
+		$(this).height(canaryHeight + 20);
+		if ($(this).hasClass('bg3') || $(this).hasClass('bg4'))
+			$(this).height(canaryHeight + 40);
+	});
+	
+}
+
+$(function() {
+	setTextAreaHeight();
+	setParallaxHeight();
+	$(window).on('resize', () => {
+		setTextAreaHeight();
+		setParallaxHeight();
+	});
+});
